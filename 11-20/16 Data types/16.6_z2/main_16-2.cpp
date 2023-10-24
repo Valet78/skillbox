82fd9534f772c994df5cfe6a9875e229b81f67e1
Valet78 <valet78@mail.ru>
2023-10-24 13:40:08 +0300

    Решение 2 задания 16 модуля.

---
+++ 11-20/16 Data types/16.6_z2/main_16-2.cpp
@@ -0,0 +1,71 @@
+// Задание 16.6.2. Сшиватель дробных чисел
+
+# include<iostream>
+// # include<limits>
+
+bool ValidNum(std::string, std::string);                 // Проверка валидности ввода
+std::string CheckInputOfEmpty(std::string); // Проверка ввода строки и пустого ввода
+std::string InputNumber(std::string, std::string);       // Ввод числа (возвращает строку)
+
+int main(){
+    std::cout << std::endl << "The program will combine the integer and fractional parts of the number." << std::endl;
+    std::string wholePart = InputNumber("Enter the whole part: ", "whole");
+    std::string fractPart = InputNumber("Specify the fractional part: ", "fract");
+    double inputNum = std::stod(wholePart + "." + fractPart);
+    
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "Input number: " << std::to_string(inputNum) << std::endl;
+
+    return 0;
+}
+
+// ****************
+
+// Проверка валидности ввода
+bool ValidNum(std::string inTxt, std::string type){                    
+    bool res = (inTxt.empty()) ? false: true;  
+    int point = 0;  
+    
+    for(int i = 0; i < inTxt.length(); i++){  
+        
+        if(i == 0 && inTxt[i] == '-'){
+           res &= (type == "whole") ? true: false;
+            continue;
+        } 
+
+        res &= inTxt[i] >= '0' && inTxt[i] <= '9';
+    }
+
+    return res;
+}
+
+// Проверка ввода строки и пустого ввода
+std::string CheckInputOfEmpty(std::string inTxt){
+  std::string result = "";
+   do{
+    std::cout << inTxt;
+    getline(std::cin, result);
+    
+    if (result.empty()){
+      std::cerr << "You forgot to enter the value! Try again." << std::endl;
+    } 
+  } while(result.empty()); 
+  return result;
+}
+
+// Ввод числа
+std::string InputNumber(std::string inTxt, std::string type){
+    std::string resTxt = "";      
+    
+    do {
+        resTxt = CheckInputOfEmpty(inTxt);               
+
+        if(!ValidNum(resTxt, type)){
+            std::cerr << "Incorrect data has been entered! Try again." << std::endl;
+        }
+
+    } while (!ValidNum(resTxt, type));
+    
+    return resTxt;
+} 
+
