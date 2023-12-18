4cca3f2ad095c5cb4169c1ec2cbc07f3494ef922
ValetVit <valet78@mail.ru>
2023-12-18 15:33:59 +0300

    Коррекция 3 задания 17 модуля.

--- 11-20/17 Pointers/17.4_z3/main_17-3.cpp
+++ 11-20/17 Pointers/17.4_z3/main_17-3.cpp
@@ -3,19 +3,44 @@
 #include<iostream>
 #include<cstring>
 
-bool ChekSubstr(const char*, const char*);
+bool chekSubstr(const char*, const char*);
 
-int main(){
+int main() {
     const char* a = "Hello world";
     const char* b = "wor";
     const char* c = "banana";
 
     std::cout.setf(std::cout.boolalpha);    // Вывод "true" и "false" вместо "1" и "0" соответственно 
-    std::cout << std::endl << ChekSubstr(a, b) << " " << ChekSubstr(a, c) << std::endl;
+    std::cout << std::endl << chekSubstr(a, b) << " " << chekSubstr(a, c) << std::endl << std::endl;
 
     return 0;
 }
 
-bool ChekSubstr(const char* inOne, const char* inTwo){
-    return (strstr(inOne, inTwo)) ? true: false;
+bool chekSubstr(const char* inOne, const char* inTwo) {
+    int i = 0, k = 0; 
+    bool res = true;    
+
+    while(*(inOne + i) != '\0') {
+
+        if(*(inOne + i) == *(inTwo + k)) {
+
+            while(*(inTwo + k) != '\0') {
+                res &= (*(inOne + i + k) != '\0' && *(inOne + i + k) == *(inTwo + k)) ? true : false;
+                k++;                
+            }
+
+            if(res) {
+                return true;
+
+            } else {
+                k = 0;
+                i = i + k;
+                res = true;
+            }
+        }
+
+        i++;
+    }
+
+    return false;
 }
