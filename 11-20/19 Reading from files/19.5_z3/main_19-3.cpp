98152db00a749e659492e88c6cba7d69ab1fb919
Valet78 <valet78@mail.ru>
2024-01-12 18:39:53 +0300

    Коррекция 3 задания 19 модуля.

--- 11-20/19 Reading from files/19.5_z3/main_19-3.cpp
+++ 11-20/19 Reading from files/19.5_z3/main_19-3.cpp
@@ -5,47 +5,50 @@
 #include <vector>
 
 std::string getPath(std::string);   // Получение полного пути (с преобразованием) к папке с файлами
-bool fileExist(std::string);        // Проверяет наличие файла
-void vecFillData(const std::string &, std::vector<std::vector<std::string>> &); // Заполнение вектора данными
-void printVec(std::vector<std::vector<std::string>> const &);      // Печать вектора
+bool validNum(std::string);         // Проверка ввода числа
 
-int main() {
-    std::vector <std::vector<std::string>> vecData;
-    std::string pathFull = getPath(__FILE__);       // Полный путь к исполняемому файлу включая его имя
-    std::string fileName = "tabl.txt";
-    pathFull += fileName;
-    int sumMoney = 0, maxMoney = 0, indMax = 0, tmp = 0;
+int main(int argc, char* argv[]) {    
+    std::string pathFull = getPath(argv[0]) + "tabl.txt";       // Полный путь к файлу включая его имя    
+    int sumMoney = 0, maxMoney = 0;
+    std::string nameFirst = "", nameLast = "", moneyTxt = "", dateTxt = "";
+    std::string nameFirstMax = "", nameLastMax = "", moneyTxtMax = "", dateTxtMax = "";
 
-    if(!fileExist(pathFull)) {
-        std::cout << std::endl;
-        std::cout << "The file \"" << fileName << "\" was not found! The program will be closed.";
+    std::ifstream file(pathFull, std::ios::in | std::ios::binary);
+    
+    if(!file.is_open()) {
+        file.close();
+        std::cerr << std::endl << "The file was not found! The program will be closed.";
         std::cout << std::endl << std::endl;
         system("Pause");
         return -1;
+    }         
 
-    } else {
-                
-        vecFillData(pathFull, vecData);
-
-        for(int i = 0; i < vecData.size(); i++) {
-            tmp = std::stoi(vecData[i][2]);
-            sumMoney += tmp;
-
-            if(tmp > maxMoney) {
-                indMax = i;
-                maxMoney = tmp;
-            }
-        }    
+    while (!file.eof()) { 
+        file >> nameFirst >> nameLast >> moneyTxt >> dateTxt;
         
-        std::cout << "*****************************************" << std::endl;
-        std::cout << "The amount of payments: " << sumMoney << std::endl;
-        std::cout << vecData[indMax][0] << " " << vecData[indMax][1] << " has the maximum payout: " << maxMoney;
-        std::cout << std::endl << std::endl;
+        if (validNum(moneyTxt)) {
+            sumMoney += stoi(moneyTxt);
 
-        // printVec(vecData);
+            if (stoi(moneyTxt) > maxMoney) {
+                nameFirstMax = nameFirst;
+                nameLastMax = nameLast;
+                moneyTxtMax = moneyTxt;
+                dateTxtMax = dateTxt;
+                maxMoney = stoi(moneyTxt);
+            }        
+        } else {
+            std::cerr << std::endl << "The data in the file is incorrect! The program will be closed.";
+            std::cout << std::endl << std::endl;
+            system("Pause");
+            return -1;
+        }
+    }
 
-    }    
-    
+    file.close();
+    std::cout << std::endl << "*****************************************" << std::endl;
+    std::cout << "The amount of payments: " << sumMoney << std::endl;
+    std::cout << nameFirstMax << " " << nameLastMax << " has the maximum payout: " << moneyTxtMax;
+    std::cout << std::endl << std::endl;    
     system("Pause");
     return 0;
 }
@@ -58,60 +61,27 @@ std::string getPath(std::string inTxt) {
     int ind = inTxt.find_last_of((char) 92) + 1;
     inTxt = inTxt.substr(0, ind);
 
-    for(int i = 0; i < inTxt.size(); i++) {
-            if(inTxt[i] == (char)92) {
-                resTxt.push_back((char)92);
-                resTxt.push_back((char)92);           
-            } else {
-                resTxt.push_back(inTxt[i]);
+    if (inTxt.find((char)92) || inTxt.find((char)47)) {
+        for(int i = 0; i < inTxt.size(); i++) {
+                if(inTxt[i] == (char)92) {
+                    resTxt.push_back((char)92);
+                    resTxt.push_back((char)92);           
+                } else {
+                    resTxt.push_back(inTxt[i]);
+                }
             }
-        }
+    }
 
     return resTxt;
 }
 
-// Проверяет наличие файла
-bool fileExist(std::string inPath) {    
-    std::ifstream file (inPath);   
-    bool res = (file.is_open()) ? true : false;       
-    file.close();
+// Проверка ввода числа
+bool validNum(std::string inTxt) {
+    bool res = (inTxt.empty()) ? false: true; 
+
+    for (int i = 0; i < inTxt.size(); i++) {   
+        res &= inTxt[i] >= '0' && inTxt[i] <= '9';   
+    }
+
     return res;
-}
-
-// Заполнение вектора данными
-void vecFillData(const std::string &inFileName, std::vector<std::vector<std::string>> &inVec) {
-    std::vector <std::string> tmpVec;
-    std::string fName = "", sName = "", uDate = "", uMoney = "";
-        
-    std::fstream file;
-    file.open(inFileName);
-    std::cout << std::endl;
-
-    while (!file.eof()) {
-        file >> fName >> sName >> uMoney >> uDate;
-        tmpVec.push_back(fName);
-        tmpVec.push_back(sName);
-        tmpVec.push_back(uMoney);
-        tmpVec.push_back(uDate);
-        inVec.push_back(tmpVec);
-        tmpVec.clear();
-    }
-
-    file.close();
-}
-
-// Печать вектора
-void printVec(std::vector<std::vector<std::string>> const &inVec) {
-    std::cout << std::endl << "******* This text from file *********" << std::endl << std::endl;
-
-    for (auto rv = inVec.begin(); rv != inVec.end(); rv++) {
-
-        for (const auto &col: *rv) {
-            std::cout << col;
-        }
-        std::cout << std::endl;
-    }
-                
-    std::cout << std::endl << "*************************************" << std::endl;
 } 
-
