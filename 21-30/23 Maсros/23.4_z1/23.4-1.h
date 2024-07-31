25ad26a7b0bc03e6ac667eed9fe8a81e49017480
Valet78 <valet78@mail.ru>
2024-07-31 13:05:36 +0300

    Выполнение 1 задания 23 модуля.

--- 21-30/23 Maсros/23.4_z1/23.4-1.h
+++ 21-30/23 Maсros/23.4_z1/23.4-1.h
@@ -2,16 +2,26 @@
 
 #include <iostream>
 
-#define MONDAY 1
-#define TUESDAY 2
-#define WEDNESDAY 3
-#define THURSDAY 4
-#define FRIDAY 5
-#define SATURDAY 6
-#define SUNDAY 7
+#define MONDAY L"1"
+#define TUESDAY L"2"
+#define WEDNESDAY L"3"
+#define THURSDAY L"4"
+#define FRIDAY L"5"
+#define SATURDAY L"6"
+#define SUNDAY L"7"
 
+#define DAY_WEEK ({(numDay == L"1") ? L"понедельник": \
+                   (numDay == L"2") ? L"вторник":     \
+                   (numDay == L"3") ? L"среда":       \
+                   (numDay == L"4") ? L"четверг":     \
+                   (numDay == L"5") ? L"пятница":     \
+                   (numDay == L"6") ? L"суббота":     \
+                   (numDay == L"7") ? L"воскресение": \
+                   L"неверные данные";})
 
 // Глобальные переменные
-
+std::wstring numDay;
 
 // Функции
+
+std::wstring inputText(std::wstring &&);          // Ввод текста и проверка на пустую строку
--- 21-30/23 Maсros/23.4_z1/main_23-1.cpp
+++ 21-30/23 Maсros/23.4_z1/main_23-1.cpp
@@ -2,9 +2,49 @@
 #include "23.4-1.h"
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    system("chcp 1251");
+    std::setlocale(LC_ALL, "Russian_Russia.1251");
 
+    do {
+        std::wcout << std::endl << L"Программа выведет наименование дня недели по его порядковому номеру." << std::endl;
+        numDay = inputText(L"Укажите порядковый номер дня недели (exit - для выхода): ");
+
+
+/* Вариант 1 */
+//        if (numDay == L"exit") {
+//            std::wcout << L"Жаль, что Вы уходите. До встречи." << std::endl;
+//        } else {
+//            if (numDay == MONDAY) {
+//                std::wcout << L"Вы указали 1 день недели т.е. понедельник." << std::endl;
+//            } else if (numDay == TUESDAY) {
+//                std::wcout << L"Вы указали 2 день недели т.е. вторник." << std::endl;
+//            } else if (numDay == WEDNESDAY) {
+//                std::wcout << L"Вы указали 3 день недели т.е. среда." << std::endl;
+//            } else if (numDay == THURSDAY) {
+//                std::wcout << L"Вы указали 4 день недели т.е. четверг." << std::endl;
+//            } else if (numDay == FRIDAY) {
+//                std::wcout << L"Вы указали 5 день недели т.е. пятница." << std::endl;
+//            } else if (numDay == SATURDAY) {
+//                std::wcout << L"Вы указали 6 день недели т.е. суббота." << std::endl;
+//            } else if (numDay == SUNDAY) {
+//                std::wcout << L"Вы указали 7 день недели т.е. воскресение." << std::endl;
+//            } else {
+//                std::wcout << L"Вы указали неверные данные! Повторите снова." << std::endl;
+//            }
+//        }
+//
+/* Вариант 2 */
+        if (numDay == L"exit") {
+            std::wcout << L"Жаль, что Вы уходите. До встречи." << std::endl;
+        } else {
+
+            if ((int) numDay[0] >= 49 && (int) numDay[0] <= 55) {
+                std::wcout << L"Вы указали " << numDay << L" день недели, - " << DAY_WEEK << L"." << std::endl;
+            } else {
+                std::wcout << L"Вы указали " << DAY_WEEK << L"! Повторите снова." << std::endl;
+            }
+        }
+
+    } while (numDay != L"exit");
 
     system("pause");
     return 0;
@@ -12,3 +52,21 @@ int main() {
 
 // *************************************
 
+// Ввод текста и проверка на пустую строку
+std::wstring inputText(std::wstring &&inTxt) {
+    std::wstring resTxt;
+    bool valid;
+
+    do {
+        valid = true;
+        std::wcout << inTxt;
+        std::getline(std::wcin, resTxt);
+
+        if (resTxt.empty()) {
+            std::wcerr << L"Забыли ввести данные. Попробуйте снова." << std::endl;
+            valid = false;
+        }
+    } while (!valid);
+
+    return resTxt;
+}
