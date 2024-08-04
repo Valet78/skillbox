a9077479a5fec882015c602f2516fcfc8df5d925
Valet78 <valet78@mail.ru>
2024-08-04 18:00:02 +0300

    Попытки найти иное решение 1 задания 23 модуля.

--- 21-30/23 Maсros/23.4_z1/23.4-1.h
+++ 21-30/23 Maсros/23.4_z1/23.4-1.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <locale>
 
-#define MONDAY L"1"
-#define TUESDAY L"2"
-#define WEDNESDAY L"3"
-#define THURSDAY L"4"
-#define FRIDAY L"5"
-#define SATURDAY L"6"
-#define SUNDAY L"7"
+//#define MONDAY L"1"
+//#define TUESDAY L"2"
+//#define WEDNESDAY L"3"
+//#define THURSDAY L"4"
+//#define FRIDAY L"5"
+//#define SATURDAY L"6"
+//#define SUNDAY L"7"
 
 #define DAY_WEEK ({(numDay == L"1") ? L"понедельник": \
                    (numDay == L"2") ? L"вторник":     \
@@ -19,8 +20,19 @@
                    (numDay == L"7") ? L"воскресение": \
                    L"неверные данные";})
 
+//#define DAY ({(numDay == L"1") ? L"MONDAY": \
+//                   (numDay == L"2") ? L"TUESDAY":     \
+//                   (numDay == L"3") ? L"WEDNESDAY":       \
+//                   (numDay == L"4") ? L"THURSDAY":     \
+//                   (numDay == L"5") ? L"FRIDAY":     \
+//                   (numDay == L"6") ? L"SATURDAY":     \
+//                   (numDay == L"7") ? L"SUNDAY": \
+//                   L"Error";})
+
+
+
 // Глобальные переменные
-std::wstring numDay;
+std::wstring numDay, wer;
 
 // Функции
 
--- 21-30/23 Maсros/23.4_z1/main_23-1.cpp
+++ 21-30/23 Maсros/23.4_z1/main_23-1.cpp
@@ -8,25 +8,24 @@ int main() {
         std::wcout << std::endl << L"Программа выведет наименование дня недели по его порядковому номеру." << std::endl;
         numDay = inputText(L"Укажите порядковый номер дня недели (exit - для выхода): ");
 
-
 /* Вариант 1 */
 //        if (numDay == L"exit") {
 //            std::wcout << L"Жаль, что Вы уходите. До встречи." << std::endl;
 //        } else {
 //            if (numDay == MONDAY) {
-//                std::wcout << L"Вы указали 1 день недели т.е. понедельник." << std::endl;
+//                std::wcout << L"Вы указали 1 день недели, т.е. понедельник." << std::endl;
 //            } else if (numDay == TUESDAY) {
-//                std::wcout << L"Вы указали 2 день недели т.е. вторник." << std::endl;
+//                std::wcout << L"Вы указали 2 день недели, т.е. вторник." << std::endl;
 //            } else if (numDay == WEDNESDAY) {
-//                std::wcout << L"Вы указали 3 день недели т.е. среда." << std::endl;
+//                std::wcout << L"Вы указали 3 день недели, т.е. среда." << std::endl;
 //            } else if (numDay == THURSDAY) {
-//                std::wcout << L"Вы указали 4 день недели т.е. четверг." << std::endl;
+//                std::wcout << L"Вы указали 4 день недели, т.е. четверг." << std::endl;
 //            } else if (numDay == FRIDAY) {
-//                std::wcout << L"Вы указали 5 день недели т.е. пятница." << std::endl;
+//                std::wcout << L"Вы указали 5 день недели, т.е. пятница." << std::endl;
 //            } else if (numDay == SATURDAY) {
-//                std::wcout << L"Вы указали 6 день недели т.е. суббота." << std::endl;
+//                std::wcout << L"Вы указали 6 день недели, т.е. суббота." << std::endl;
 //            } else if (numDay == SUNDAY) {
-//                std::wcout << L"Вы указали 7 день недели т.е. воскресение." << std::endl;
+//                std::wcout << L"Вы указали 7 день недели, т.е. воскресение." << std::endl;
 //            } else {
 //                std::wcout << L"Вы указали неверные данные! Повторите снова." << std::endl;
 //            }
@@ -38,13 +37,14 @@ int main() {
         } else {
 
             if ((int) numDay[0] >= 49 && (int) numDay[0] <= 55) {
-                std::wcout << L"Вы указали " << numDay << L" день недели, - " << DAY_WEEK << L"." << std::endl;
+                std::wcout << L"Вы указали " << numDay << L" день недели, т.е. " << DAY_WEEK << L"." << std::endl;
             } else {
                 std::wcout << L"Вы указали " << DAY_WEEK << L"! Повторите снова." << std::endl;
             }
         }
 
     } while (numDay != L"exit");
+
 
     system("pause");
     return 0;
