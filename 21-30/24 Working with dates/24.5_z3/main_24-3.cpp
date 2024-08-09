b477cfe27252da77632625118042e5e2d8deb090
Valet78 <valet78@mail.ru>
2024-08-09 14:52:56 +0300

    Добавил задания 24 модуля.

---
+++ 21-30/24 Working with dates/24.5_z3/main_24-3.cpp
@@ -0,0 +1,13 @@
+// Задание 24.5.3. Реализация программы таймера
+#include "24.5-3.h"
+
+int main() {
+    std::setlocale(LC_ALL, "Russian_Russia.1251");
+
+
+    system("pause");
+    return 0;
+}
+
+// *************************************
+
