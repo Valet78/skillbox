69468fd36a960f401f61033e7a0d3e52ed9ba605
Valet78 <valet78@mail.ru>
2024-08-28 15:06:30 +0300

    Добавил задания 25 модуля.

---
+++ 21-30/25 Project structure/25.4_z1/src/main_25-1.cpp
@@ -0,0 +1,14 @@
+// Задание 23.4.1. 
+#include "../include/25.4-1.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+
+    system("pause");
+    return 0;
+}
+
+// *************************************
+
